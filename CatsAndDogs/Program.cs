cc49103ef27541adf5b6324d5d954c2a9cc52a58
Jaagup Vaar <jaagupvaar@MacBook-Air.lan>
2022-03-31 15:01:00 +0300

    add new projects

---
+++ CatsAndDogs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CatsAndDogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Are you more like a cat or a dog?");
+            string userChoice = Console.ReadLine();
+
+            if(userChoice == "Cat")
+            {
+                Console.WriteLine("You are a home-lover.");
+
+            }
+            else if (userChoice == "Dog")
+            {
+                Console.WriteLine("You are a partygoer.");
+
+            }
+            else
+            {
+                    Console.WriteLine($"You are a {userChoice} friend.");
+             
+            }
+
+        }
+    }
+}
---
+++ PersonalityTestSwitch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PersonalityTestSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm  palub kasutajal siestada oma lemmikvärv;
+            //kui kasutaja sisestab "punane", konsool kuvab "oled romantiline";
+            //kui kasutaja sisestab "sinine", konsool kuvab "oled töökas";
+            //kui kasutaja sisestab "rohleine", kunsool kuvab "oled looduse sõber";
+            //kui kasutaja sisestab midagi muud, konsool kuvab "oled {userColor} ükssarvik";
+            Console.WriteLine("Sisesta oma lemmikvärv:");
+            string userColor = Console.ReadLine().ToLower();
+
+            //Punane, Sinine, Roheline
+
+            switch(userColor)
+            {
+                case "punane":
+                    Console.WriteLine("Oled romantiline.");
+                    break;
+                case "roheline":
+                    Console.WriteLine("Oled looduse sõber.");
+                    break;
+                case "sinine":
+                    Console.WriteLine("Oled töökas.");
+                    break;
+                default:
+                    Console.WriteLine($"Oled {userColor} ükssarvik.");
+                    break;
+            }
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
